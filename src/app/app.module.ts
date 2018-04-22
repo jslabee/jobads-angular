a7a420a75cdd2ad4e34211d3da95036847776b7a
jslabee <jslabe@gmail.com>
2018-04-22 23:14:01 +0200

    delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { Http, Response, RequestOptions, Headers, HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { AdsComponent } from './ads/ads.component';
 import { Ng2OrderModule } from 'ng2-order-pipe';
 import { OrderModule } from 'ngx-order-pipe';
 import { DataService } from './services/data--.service';
@@ -12,7 +11,6 @@ import { DataService } from './services/data--.service';
 @NgModule({
   declarations: [
     AppComponent,
-    AdsComponent
   ],
   imports: [
     BrowserModule,
